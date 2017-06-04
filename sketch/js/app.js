3de9c318e22dd6c1cf90ed3e81e6f7d027dcb477
pkManee <pk.manee@gmail.com>
2017-06-04 10:46:21 +0700

    Add files via upload
    big fix
    - shirt not disappear when switching mode from design mode to shirt mode

--- sketch/js/app.js
+++ sketch/js/app.js
@@ -103,7 +103,9 @@ function toggleMode(){
     var panelColor = document.getElementById('panel-color');    
     //to shirt mode
     if (isShirtMode){
+        alert('switching to shirt mode')
         canvas.wrapperEl.style.zIndex = -1;
+        shirtCanvas.wrapperEl.style.display = 'inline-block';
         shirtCanvas.wrapperEl.style.zIndex = 1;
         drawTools.style.display = 'none';
         panelColor.style.display = 'inline-block';
@@ -118,8 +120,10 @@ function toggleMode(){
         // }
     }else{
     //to design mode
+        alert('switching to design mode')
         canvas.wrapperEl.style.zIndex = 1;
         shirtCanvas.wrapperEl.style.zIndex = -1;
+        shirtCanvas.wrapperEl.style.display = 'none';
         drawTools.style.display = 'inline-block';
         panelColor.style.display = 'none';
         //toggle tab page
@@ -1401,90 +1405,3 @@ function init() {
    
 }//init
 init();
-
-// function snn(img, radius) {
-// var _ctx = document.createElement('canvas').getContext('2d');
-// var ctx = document.createElement('canvas').getContext('2d');
-// var imgdata;
-
-// ctx.canvas.width = img.width;
-// ctx.canvas.height = img.height;
-// ctx.drawImage(img, 0, 0);
-// imgdata = ctx.getImageData(0, 0, img.width, img.height);
-
-// var data = imgdata;
-// var srcimg = data,
-//     w = srcimg.width,
-//     h = srcimg.height,
-//     r = parseInt(radius),
-//     div = 1.0/((2*r + 1)*(2*r + 1)),
-//     srcdata = srcimg.data,
-//     dstimg = _ctx.createImageData(w, h),
-//     dstdata = dstimg.data,
-//     sumr, sumg, sumb,
-//     rc, gc, bc, r1, g1, b1, r2, g2, b2,
-//     pv, pu, xystep, uvstep, delta1, delta2,
-//     i, j;
-
-// for(var y=0; y<h; y++) {
-//     xystep = y*w;
-//     for(var x=0; x<w; x++) {
-//       i = (xystep + x) << 2;
-//       sumr = 0, sumg = 0, sumb = 0;
-//       rc = srcdata[i];
-//       gc = srcdata[i + 1];
-//       bc = srcdata[i + 2];
-//       for(var v=-r; v<=r; v++) {
-//         uvstep = w*v;
-//         for(var u=-r; u<=r; u++) {
-//           j = (uvstep + u) << 2;
-//           if(srcdata[i + j]) {
-//             r1 = srcdata[i + j];
-//             g1 = srcdata[i + j + 1];
-//             b1 = srcdata[i + j + 2];
-//           } else {
-//             r1 = srcdata[i];
-//             g1 = srcdata[i + 1];
-//             b1 = srcdata[i + 2];
-//           }
-//           if(srcdata[i - j]) {
-//             r2 = srcdata[i - j];
-//             g2 = srcdata[i - j + 1];
-//             b2 = srcdata[i - j + 2];
-//           } else {
-//             r2 = srcdata[i];
-//             g2 = srcdata[i + 1];
-//             b2 = srcdata[i + 2];
-//           }
-//           delta1 = Math.sqrt((rc - r1)*(rc - r1) +
-//                              (gc - g1)*(gc - g1) +
-//                              (bc - b1)*(bc - b1));
-//           delta2 = Math.sqrt((rc - r2)*(rc - r2) +
-//                              (gc - g2)*(gc - g2) +
-//                              (bc - b2)*(bc - b2));
-//           if(delta1 < delta2) {
-//             sumr += r1;
-//             sumg += g1;
-//             sumb += b1;
-//           } else {
-//             sumr += r2;
-//             sumg += g2;
-//             sumb += b2;
-//           }
-//         }
-//       }
-//       dstdata[i] = sumr*div;
-//       dstdata[i + 1] = sumg*div;
-//       dstdata[i + 2] = sumb*div;
-//       dstdata[i + 3] = 255;
-//     }
-// }
-//     //return dstimg;
-
-//     var c = document.createElement('canvas');
-//     c.width = img.width;
-//     c.height = img.height;
-//     c.getContext('2d').putImageData(dstimg, 0, 0);
-   
-//     return c.toDataURL();
-// }
